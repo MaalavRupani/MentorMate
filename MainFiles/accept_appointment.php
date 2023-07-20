f35a86840f76aaff65cb50c11083af7328f1e275
Maalav <rupani.maalav@gmail.com>
2023-07-20 18:06:11 +0530

    Create accept_appointment.php

---
+++ MainFiles/accept_appointment.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once "config.php";
+
+    session_start();
+
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: mentor_login.php");
+        exit;
+    }
+
+    $appointment_id = $_GET['appointment_id'];
+
+    $sql = "update appointment set appointment_status = 1, appointment_accepted = 1 where appointment_id = '$appointment_id'";
+    
+    if (mysqli_query($link, $sql))
+    {
+        echo "<script> window.alert('Appointment Accepted');
+        window.location.href='mentor_welcome.php';</script>";
+    }
+?>
